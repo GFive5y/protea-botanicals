34c3ddccd5940fab31462510fceacef9c9d8b905
G5 <fivazg@gmail.com>
2026-02-19 02:49:48 +1300

    Stage 2: Supabase connection tested

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { supabase } from "./services/supabaseClient";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+useEffect(() => {
+  supabase
+    .from("batches")
+    .select("*")
+    .then(({ data, error }) => {
+      console.log("Test data:", data, "Error:", error);
+    });
+}, []);
