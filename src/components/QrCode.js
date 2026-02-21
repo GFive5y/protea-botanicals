407178c98c6781b6e1c887b4f10190afdf72d2b8
G5 <fivazg@gmail.com>
2026-02-21 13:25:31 +1300

    Fix qrcode.react import for Netlify build

--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -1,8 +1,7 @@
-// src/components/QrCode.js - FIXED TO REDIRECT TO LIVE SITE
-import QRCode from "qrcode.react";
+// src/components/QrCode.js - FIXED FOR NETLIFY BUILD
+import { QRCodeCanvas as QRCode } from "qrcode.react";
 
 export default function QrCode({ value, size = 128 }) {
-  // Change to full live URL so phone camera opens the site
   const fullUrl = `https://protea-botanicals.netlify.app/scan/${value}`;
 
   return <QRCode value={fullUrl} size={size} level="H" includeMargin={true} />;
